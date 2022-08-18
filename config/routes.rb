079b0c7144de011d7d956f4655a985adfa308daa
Joao Henriques <joao33ti@gmail.com>
2022-08-18 15:07:00 +0200

    exo2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root to: 'lists#index'
+  get 'about', to: 'pages#about'
+  resources :lists, only: [:index, :show, :new, :create]
   # Defines the root path route ("/")
   # root "articles#index"
 end
