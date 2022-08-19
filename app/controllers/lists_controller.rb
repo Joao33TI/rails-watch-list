344864c8f7aa8d5e9f899d5080cb7185941e3063
Joao Henriques <joao33ti@gmail.com>
2022-08-19 14:41:46 +0200

    WIP

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,4 +2,21 @@ class ListsController < ApplicationController
   def index
     @lists = List.all
   end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = list.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
 end
